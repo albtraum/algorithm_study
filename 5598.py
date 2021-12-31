2145f0e7b6618021af4cf4ae611cc823566c36ab
jackbae <44794874+jackbae@users.noreply.github.com>
2021-12-31 22:50:05 +0900

    Delete 5598.py

--- 5598.py
+++
@@ -1,9 +0,0 @@
-# for i in list(input()):
-#     print(chr(ord(i)-3) if ord(i)>=68 else chr((ord(i)+23)), end="")
-
-# x = list(input())
-# for i in x:
-#     if ord(i)>=68:
-#         print(chr(ord(i)-3), end="")
-#     else:
-#         print(chr(ord(i)+23), end="")
