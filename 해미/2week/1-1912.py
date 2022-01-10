61528df36b4b3cca008ab761e00a0837c17d9de8
parkhaemi <haemipark21@gmail.com>
2022-01-10 23:18:39 +0900

    2주차 월요일 문제 수정

--- 해미/2week/1-1912.py
+++ 해미/2week/1-1912.py
@@ -6,5 +6,5 @@ num = list(map(int, sys.stdin.readline().split()))
 arr = [0]*N 
 arr[0] = num[0] 
 for i in range(1,N): 
-    arr[i] = max(arr[i-1]+num[i], num[i]) #지금 위치의 값을 전의 값에 현재값을 연산한 것으로 바꿀지, 아니면 현재 값으로 유지할지 계산
+    arr[i] = max(arr[i-1]+num[i], num[i]) #지금 위치의 값을 전의 값에 현재값을 연산한 것으로 바꿀지, 아니면 현재 값으로 유지할지(새로 시작할지) 계산
 print(max(arr))
