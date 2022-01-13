0ab5f138e71c33b2164f1f3eef2b226bf36d5ec1
jackbae <44794874+jackbae@users.noreply.github.com>
2022-01-13 21:41:20 +0900

    Add files via upload

---
+++ 재현/2week/3-2164.py
@@ -0,0 +1,11 @@
+input = int(input())
+square = 2
+
+while True:
+    if (input == 1 or input == 2):
+        print(input)
+        break 
+    square *= 2
+    if (square >= input):
+        print((input - (square // 2)) * 2)
+        break
